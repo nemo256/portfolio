41fb12ab9d9154a02b48b2fc205d0229657e41da
nemo256 <neggazimedlamine@gmail.com>
2021-05-01 22:33:16 +0100

    Fixed favicon icon and made it more responsive on mobile!

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,12 +2,13 @@ import Head from 'next/head'
 import Navbar from './Navbar'
 import Footer from './Footer'
 import { Box } from '@chakra-ui/react'
+import { FaHandPeace } from 'react-icons/fa'
 
 export default function Layout({ children }) {
   return (
     <>
       <Head>
-        <link rel="icon" href="wave-emoji.svg" />
+        <link rel='icon' href='favicon.svg'/>
         <title>Amine Neggazi</title>
       </Head>
       <Box
