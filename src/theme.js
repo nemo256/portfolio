e69584389b2e4673fdd00260354c260d2debf805
nemo256 <neggazimedlamine@gmail.com>
2021-05-12 23:44:57 +0100

    Custom background color throughout the website!

--- src/theme.js
+++ src/theme.js
@@ -1,9 +1,12 @@
 import { extendTheme } from "@chakra-ui/react"
 
 const theme = extendTheme({
+  config: {
+    bgColor: 'black',
+  },
   fonts: {
-    heading: "Roboto Mono",
-    body: "Roboto Mono",
+    heading: 'Roboto Mono',
+    body: 'Roboto Mono',
   },
 })
 
