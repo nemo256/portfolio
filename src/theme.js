2231c927562f3ed59daa09fc4fb2fa1c8b7b015c
nemo256 <neggazimedlamine@gmail.com>
2021-05-12 23:47:52 +0100

    Custom global theme style props!

--- src/theme.js
+++ src/theme.js
@@ -1,12 +1,18 @@
 import { extendTheme } from "@chakra-ui/react"
 
 const theme = extendTheme({
-  config: {
-    bgColor: 'black',
-  },
   fonts: {
     heading: 'Roboto Mono',
     body: 'Roboto Mono',
+  },
+  styles: {
+    global: {
+      // styles for the `body`
+      body: {
+        bg: "black",
+        color: "white",
+      },
+    },
   },
 })
 
